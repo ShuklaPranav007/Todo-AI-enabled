d611970a2453bbe410ac69c3f71298c482fa8c89
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-03-21 15:24:16 +0530

    model todo

---
+++ backend/models/Todo.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const todoSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      type: String,
+      enum: ['low', 'medium', 'high'],
+      default: 'medium',
+    },
+    dueDate: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+)
+
+const Todo = mongoose.model('Todo', todoSchema)
+
+export default Todo
